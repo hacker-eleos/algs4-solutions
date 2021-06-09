1890dbcec5c0f6fab4525b13445a51c3264a7c69
Vikram bhatt <vikrambhatt1405@gmail.com>
2021-06-09 17:38:45 +0530

    Closest pair of point with O(n lgn) algorithm) done.

--- ch1/section2/src/Exercise1.java
+++ ch1/section2/src/Exercise1.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.Counter;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.StdRandom;
 
@@ -16,6 +17,7 @@ public class Exercise1 {
             points[i] = new Point2D(x, y);
         }
         System.out.println(closestPairDistanceBruteForce(points));
+        System.out.println(closestPairDistance(points));
     }
 
     private static void preProcess(Point2D[] points) {
@@ -40,20 +42,37 @@ public class Exercise1 {
         Point2D[] lx = Arrays.copyOfRange(px, 0, mid);
         Point2D[] rx = Arrays.copyOfRange(px, mid, N);
         Point2D[] ly = new Point2D[lx.length];
-        Point2D[] ry = new Point2D[ly.length];
+        Point2D[] ry = new Point2D[rx.length];
         int j = 0;
         int k = 0;
         for (int i = 0; i < N; i++) {
-            if (py[i].x() <= px[mid].x()) ly[j++] = new Point2D(py[i].x(), py[i].y());
+            if (py[i].x() < px[mid].x()) ly[j++] = new Point2D(py[i].x(), py[i].y());
             else ry[k++] = new Point2D(py[i].x(), py[i].y());
         }
         double leftDistance = closestPairDistanceAux(lx, ly);
         double rightDistance = closestPairDistanceAux(rx, ry);
-        return 0;
+        double closestSplitDistance = closestSplitPair(px, py, Math.min(leftDistance, rightDistance));
+        return Math.min(leftDistance, Math.min(rightDistance, closestSplitDistance));
     }
 
-    private static double closestSplitPair(Point2D[] px, Point2D[] py){
-        return 0;
+    private static double closestSplitPair(Point2D[] px, Point2D[] py, double delta) {
+        double median = px[px.length / 2].x();
+        Counter counter = new Counter("points in Sy");
+        for (int i = 0; i < py.length; i++) {
+            if ((median - delta <= py[i].x()) && (py[i].x() <= median + delta)) counter.increment();
+        }
+        Point2D[] sy = new Point2D[counter.tally()];
+        for (int i = 0, j = 0; i < py.length; i++) {
+            if ((median - delta <= py[i].x()) && (py[i].x() <= median + delta)) sy[j++] = py[i];
+        }
+        double minDistance = delta;
+        for (int i = 0; i < sy.length; i++) {
+            for (int k = 1; k < Math.min(7, sy.length - i); k++) {
+                if (sy[i].distanceTo(sy[i + k]) < minDistance)
+                    minDistance = sy[i].distanceTo(sy[i + k]);
+            }
+        }
+        return minDistance;
     }
 
     private static double closestPairDistanceBruteForce(Point2D[] points) {
@@ -69,13 +88,4 @@ public class Exercise1 {
         return minDistance;
     }
 
-    private class Pair {
-        private final Point2D x;
-        private final Point2D y;
-
-        public Pair(Point2D a, Point2D b) {
-            x = new Point2D(a.x(), a.y());
-            y = new Point2D(b.x(), b.y());
-        }
-    }
 }
